844a4adf0b2e36a93b5bc657bff0f563b15da37a
Ridwan Balogun <balogunridwan@gmail.com>
2019-11-12 18:32:32 +0100

    Webpack updated

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:{
+                    
+                }
+            }
+        ]
+    }
+}
